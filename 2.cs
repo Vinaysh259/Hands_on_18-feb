5bc0f60851e0c7e0b459a46d7f69cc1dff3ce6d4
reshmaroy1801 <reshmaroy1801@gmail.com>
2019-02-18 20:12:01 +0530

    modified 2

--- 2.cs
+++ 2.cs
@@ -1,11 +1,12 @@
 using System;
 
-class Count{
-	static void Main(){
-		string str;
-		Console.WriteLine("Enter the  new String:demo \n");
-		
-	}
+class Count
+{
+    static void Main()
+    {
+        string str;
+        Console.WriteLine("Enter the  new String: \n");
+
+    }
 }
-	}
-}
+	
