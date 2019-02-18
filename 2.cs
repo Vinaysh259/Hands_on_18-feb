ea88d3e565a131d8f9f4a6755a703a1b8c3bcf7e
reshmaroy1801 <47671483+reshmaroy1801@users.noreply.github.com>
2019-02-18 20:27:41 +0530

    Update 2.cs

--- 2.cs
+++ 2.cs
@@ -5,7 +5,7 @@ class Count
     static void Main()
     {
         string str;
-        Console.WriteLine("Enter the  new String:demo \n");
+        Console.WriteLine("Enter the  new String: \n");
 
     }
 }
