d15fe5774013644e913bbe69bdb01288da544dfb
Vinay <vinaysh259@gmail.com>
2019-02-19 14:18:05 +0530

    5th program is added

--- 5.cs
+++ 5.cs
@@ -1,5 +1,4 @@
 using System;
-
 namespace String
 {
     class program5
@@ -9,18 +8,22 @@ namespace String
             string str1;
             char[] ch_arr1;
             int l_1;
+            string a;
+            int i, l;
+            char[] arr;
+
             Console.WriteLine("Welcome to Reversing the string Application:");
             Console.WriteLine("Enter the String :");
             str1 = Console.ReadLine();
             l_1 = str1.Length;
-            ch_arr1 = str1.ToCharArray(0,l_1);
+            ch_arr1 = str1.ToCharArray(0, l_1);
             Array.Reverse(ch_arr1);
             Console.WriteLine("Reversed string is :");
             Console.WriteLine(ch_arr1);
             Console.ReadKey();
 
             Console.WriteLine("\n\n\n\n");
-            string str3,str5;
+            string str3, str5;
             string ch1;
             char[] ch_arr3;
             int l_3;
@@ -34,6 +37,37 @@ namespace String
             str5 = str3.Replace(ch1[0], '$');
             Console.WriteLine(str5);
             Console.ReadKey();
+
+            Console.WriteLine("Enter the input String");
+            a = Console.ReadLine();
+            l = a.Length;
+            //arr1 = a.ToCharArray(0, l);
+            string sub = a.Substring(1, l - 1);
+            Console.WriteLine("Substring: {0}\n\n\n\n\n", sub);
+
+
+            string a2 = string.Copy(a);
+            arr = a2.ToCharArray(0, l);
+            //string output = a2.Replace(a2, "* ");
+            Console.WriteLine("::BEFORE::");
+            Console.WriteLine(a2);
+            Console.WriteLine("::AFTER::");
+            for (i = 0; i < l; i++)
+            {
+                char ch = arr[i];
+                if (Char.IsLower(ch))
+                    Console.Write(Char.ToUpper(ch));
+                else
+                    Console.Write(Char.ToLower(ch));
+            }
+            //string output = a2.Replace(a2, "* ");
+
+            //Console.WriteLine(output);
+
+            Console.ReadKey();
         }
     }
 }
+
+
+
