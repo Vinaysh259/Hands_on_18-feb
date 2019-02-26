cd84bc37bf76b842bbd44e686cf2a8bd08fb04c0
reshmaroy1801 <reshmaroy1801@gmail.com>
2019-02-26 16:31:55 +0530

    more files added

---
+++ TM-02/1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace _1
+{
+    class Car
+    {
+        private string carmake;
+        private string model;
+        private int mfgYear;
+        private double price;
+
+        public Car(string a,string b,int c,double d)
+        {
+            carmake = a;
+            model = b;
+            mfgYear = c;
+            price = d;
+        }
+        public void DisplayCar()
+        {
+            Console.WriteLine("details of the car:");
+            Console.WriteLine("carmake={0}\nmodel={1}\nmfgYear={2}\nprice={3}", carmake, model, mfgYear, price);
+        }
+        static void Main()
+        {
+            Console.WriteLine("enter the details of the car");
+            string carmake1 = Console.ReadLine();
+            string model1 = Console.ReadLine();
+            int mfgYear1 = Convert.ToInt32(Console.ReadLine());
+            double price1 = double.Parse(Console.ReadLine());
+
+            Car car = new Car(carmake1,model1,mfgYear1,price1);
+
+          
+            car.DisplayCar();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TM-02/3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace _1
+{
+    class Stock
+    {
+        private string StockName;
+        private string StockSymbol;
+        private double PreviousClosingPrice;
+        private double CurrentClosingPrice;
+        public Stock(string a, string b, double c, double d)
+        {
+            StockName = a;
+            StockSymbol = b;
+            PreviousClosingPrice = c;
+            CurrentClosingPrice = d;
+        }
+        public void GetChangePercentage()
+        {
+            double percentage = ((PreviousClosingPrice - CurrentClosingPrice) / PreviousClosingPrice) * 100;
+            Console.WriteLine("percentage change is:{0}%",percentage);
+            
+        }
+        static void Main()
+        {
+            Console.WriteLine("enter the stock details");
+            string StockName = Console.ReadLine();
+            string StockSymbol = Console.ReadLine();
+            double PreviousClosingPrice = double.Parse(Console.ReadLine());
+            double CurrentClosingPrice = double.Parse(Console.ReadLine());
+            Stock stock = new Stock(StockName, StockSymbol, PreviousClosingPrice, CurrentClosingPrice);
+
+
+            stock.GetChangePercentage();
+            Console.ReadKey();
+        }
+    }
+}
