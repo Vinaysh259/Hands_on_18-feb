227cfefeee230ad83ffd16fc971bd117228cdffe
reshmaroy1801 <reshmaroy1801@gmail.com>
2019-03-16 21:06:50 +0530

    more problems added

--- TM-04/2.11.cs
+++ TM-04/2.11.cs
@@ -1,5 +1,8 @@
-using System;
-
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace program11
 {
@@ -8,16 +11,18 @@ namespace program11
         static void Main(string[] args)
         {
             int[] a = { 1, 2, 3, 5, 7 };
-            int count = 0, l, i;
+            int count = 0,l,i;
+            ArrayList list = new ArrayList();
             Console.WriteLine("no. of elements in a given array:");
             l = a.Length;
             for (i = 0; i < l; i++)
             {
                 count++;
             }
-
-
-            Console.WriteLine("The array contains {0} elements", count);
+            list.Add(count);
+            foreach (var val in list)
+                Console.WriteLine(val);
+            
             Console.ReadKey();
         }
     }
---
+++ TM-04/2.12.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i, n,N,j=1;
+            int[] a = new int[100];
+            int[] a_reverse = new int[100];
+            Console.WriteLine("Enter the Number of inputs : ");
+            N = int.Parse(Console.ReadLine());
+            SortedList list1 = new SortedList();
+            while (j <= N)
+            {
+                Console.Write("Input the number of elements to store in the array {0}:", j);
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Input {0} number of elements in the array :\n", n);
+                for (i = 0; i < n; i++)
+                {
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (i = 0; i < n; i++)
+                {
+                    a[i] = a[i];
+                   
+                }
+                for (i = n - 1; i >= 0; i--)
+                {
+                    a_reverse[(n-1)-i] = a[i];
+                    
+                }
+                string[] original_str = Array.ConvertAll(a, ele => ele.ToString());//converts integer array to string
+                string d = string.Join(",", original_str);
+                string[] reverse_str = Array.ConvertAll(a_reverse, ele => ele.ToString());
+                string e=string.Join(",", reverse_str);
+                list1.Add(d, e);
+                j++;
+           }
+                foreach (DictionaryEntry val in list1)
+                {
+                    Console.WriteLine("original:({0}) ,reverse: ({1})",val.Key, val.Value);
+                }
+            Console.ReadKey();
+        }
+    }
+}
