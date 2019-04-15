f6950b22de9e690ec9e70e439be623d5ae118571
reshmaroy1801 <reshmaroy1801@gmail.com>
2019-04-15 17:01:01 +0530

    6th added

---
+++ TM-05/5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] Drives = DriveInfo.GetDrives();
+            foreach (DriveInfo Drive in Drives)
+            {
+                comboBox1.Items.Add(Drive);
+            }
+            //comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string myvariable = comboBox1.Text;
+            string[] files = Directory.GetDirectories(myvariable);
+             string[] files1 = Directory.GetFiles(myvariable);
+            //var files = Directory.EnumerateFiles(myvariable);
+            foreach(var val in files)
+            {
+                listBox1.Items.Add(val);
+            }
+             foreach(var val in files1)
+            {
+                listBox1.Items.Add(val);
+            }
+        }
+    }
+}
---
+++ TM-05/6.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           // this.ActiveControl = textBox1;
+           // textBox1.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           listBox1.Items.Clear();
+            string myvariable =textBox1.Text;
+            string[] files = Directory.GetDirectories(myvariable);
+            string[] files1 = Directory.GetFiles(myvariable);
+            foreach (var val in files)
+            {
+                listBox1.Items.Add(val);
+            }
+            foreach (var val in files1)
+            {
+                listBox1.Items.Add(val);
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+    }
+}
